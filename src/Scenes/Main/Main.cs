ad68532aa578083f5dc5836948daaca781d09166
Sebastian Sommer <mail@sommer-sebastian.de>
2025-03-02 21:25:21 +0100

    Switched out the config reader for WolfAPI calls

--- src/Scenes/Main/Main.cs
+++ src/Scenes/Main/Main.cs
@@ -12,11 +12,30 @@ namespace UI
 		[Export]
 		public DockerController docker;
 		[Export]
-		public WolfConfig config;
+		public WolfAPI wolfAPI;
+		public WolfClient SelectedClient = null;
 
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
+			SoundEffects soundEffects = null;
+			foreach(var child in GetChildren())
+			{
+				if(child is SoundEffects effects)
+					soundEffects = effects;
+			}
+
+			soundEffects.CallDeferred(SoundEffects.MethodName.ApplySoundEffects, this);
+
+			var time = new Timer();
+			time.WaitTime = 0.2;
+			time.OneShot = false;
+			time.Autostart = true;
+			time.Timeout += () => {
+				soundEffects.ApplySoundEffects(this);
+			};
+
+			AddChild(time);
 		}
 	}
 }
