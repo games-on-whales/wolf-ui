a1dd2226ed4e3660d586a936b84d1640f6e5d1ba
Sebastian Sommer <mail@sommer-sebastian.de>
2025-03-13 09:13:34 +0100

    Added Dark "OLED" Theme, Added Logic to invert Dark Icons if Background is Dark Too

--- src/Scenes/Main/Header/ExitButton.cs
+++ src/Scenes/Main/Header/ExitButton.cs
@@ -6,5 +6,31 @@ public partial class ExitButton : Button
     public override void _Ready()
     {
         Pressed += () => GetTree().Quit();
+        ThemeChanged += OnThemeChanged;
+        OnThemeChanged();
+    }
+
+    private void OnThemeChanged()
+    {
+        var color = GetThemeColor("panel");
+        if(color.R < 0.3f && color.G < 0.3f && color.B < 0.3f)
+            InvertIconColor();
+    }
+    
+    private void InvertIconColor()
+    {
+        var image = Icon.GetImage();
+        for(var x = 0; x < image.GetWidth(); ++x)
+        {
+            for(var y = 0; y < image.GetHeight(); ++y)
+            {
+                var pixel = image.GetPixel(x, y);
+                pixel.R = 1.0f - pixel.R;
+                pixel.G = 1.0f - pixel.G;
+                pixel.B = 1.0f - pixel.B;
+                image.SetPixel(x, y, pixel);
+            }
+        }
+        Icon = ImageTexture.CreateFromImage(image);
     }
 }
--- src/Scenes/Main/Header/OptionsButton.cs
+++ src/Scenes/Main/Header/OptionsButton.cs
@@ -5,8 +5,32 @@ public partial class OptionsButton : Button
 {
     public override void _Ready()
     {
-        Pressed += () => {
-            GetNode<Control>("%UserList").Visible = true;
-        };
+        Pressed += () => GetNode<Control>("%UserList").Visible = true;
+        ThemeChanged += OnThemeChanged;
+        OnThemeChanged();
+    }
+
+    private void OnThemeChanged()
+    {
+        var color = GetThemeColor("panel");
+        if(color.R < 0.3f && color.G < 0.3f && color.B < 0.3f)
+            InvertIconColor();
+    }
+    
+    private void InvertIconColor()
+    {
+        var image = Icon.GetImage();
+        for(var x = 0; x < image.GetWidth(); ++x)
+        {
+            for(var y = 0; y < image.GetHeight(); ++y)
+            {
+                var pixel = image.GetPixel(x, y);
+                pixel.R = 1.0f - pixel.R;
+                pixel.G = 1.0f - pixel.G;
+                pixel.B = 1.0f - pixel.B;
+                image.SetPixel(x, y, pixel);
+            }
+        }
+        Icon = ImageTexture.CreateFromImage(image);
     }
 }
